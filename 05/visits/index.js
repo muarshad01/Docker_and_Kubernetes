4aea4bed5bf648d33e5b5c067d3d3a2c90db96e3
abc <abc@xyz.com>
2023-09-13 18:30:20 -0400

    update

--- 05/visits/index.js
+++ 05/visits/index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+	host: 'redis-server',
+	port: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-	client.get('visites', (err, visits) => {
-		res.send('Number of visits is ' + visits);
-		client.set('visits', parseInt(visits) + 1);
-	});
+    client.get('visits', (err, visits) => {
+        res.send('Number of visits is ' + visits);
+        client.set('visits', parseInt(visits) + 1);
+    });
 });
 
-app.listen(8081, ()=> {
-	console.log('Listening on port 8081');
+app.listen(8081, () => {
+    console.log('Listening on port 8081');
 });
